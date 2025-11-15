b1794ea960fd49c31be6041de9a40707c9d6c1b4
Co2-Fussabdruck <barthelniclas155@gmail.com>
2025-11-15 13:06:15 +0100

    Update switch_style.js

--- switch_style.js
+++ switch_style.js
@@ -1,30 +1,25 @@
-const themeToggle = document.getElementById("theme-toggle");
+const themeToggle = document.getElementById("colorSelector");
 let theme_color = document.querySelector(":root")
-const colors = ["#fe2712", "#fbf203", "#0247fe", "#ff7f00", "#66b032", "#800080"];
-
-let currentTheme = 0
-
 
 
 function saveTheme() {
-  localStorage.setItem("theme", currentTheme)
+  localStorage.setItem("theme", themeToggle.value)
 }
-if (isNaN(parseInt(localStorage.getItem("theme"))) == true) {
+if (localStorage.getItem("theme") == null) {
   saveTheme()
-} else {
-  currentTheme = (localStorage.getItem("theme")) % colors.length
 }
 
+themeToggle.value = localStorage.getItem("theme")
+
 function updateTheme() {
-  theme_color.style.setProperty("--theme", colors[currentTheme])
+  theme_color.style.setProperty("--theme", themeToggle.value)
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
   updateTheme()
 });
 
-themeToggle.addEventListener("click", () => {
-  currentTheme = (currentTheme + 1) % colors.length
+themeToggle.addEventListener("input", () => {
   updateTheme()
   saveTheme()
 });
